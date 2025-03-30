6a0af8baa3d6fdadddfd274649cc3d67d5baf6d5
chendeng <530972294@qq.com>
2025-03-30 22:02:45 +0800

    test: 测试用例

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,34 @@
 </template>
 
 <script setup lang="ts">
+import { astToNode, createElement } from '../lib'
 
+const node1 = createElement(
+  'div',
+  { class: 'container' },
+  createElement('div', { class: 'container' }, '子节点1'),
+  createElement('div', { class: 'container' }, '子节点2')
+)
+
+console.log('node1', node1)
+
+const node = astToNode({
+  tagName: 'div',
+  props: {
+    class: 'container'
+  },
+  children: [
+    '字符串',
+    {
+      tagName: 'div',
+      children: [
+        '内部的'
+      ]
+    }
+  ]
+})
+
+console.log('node', node)
 </script>
 
 <style scoped lang="less">
